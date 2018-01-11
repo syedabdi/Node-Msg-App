d201c5d3b0c0781fd063abf9258dd9b715d8d3ef
syedabdi <haiderofbme@gmail.com>
2018-01-10 20:19:10 -0800

    add heroku server

--- server/server.js
+++ server/server.js
@@ -7,6 +7,14 @@ var app = express();
 
 app.use(express.static(publicPath));
 
-app.listen(port,()=>
-console.log('Server is up on: '+ $(port))
+
+
+app.listen(port,()=>{
+    console.log(__dirname);
+console.log(publicPath);
+console.log('Server is up on: ')
+}
 );
+
+
+
